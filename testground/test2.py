825f3645b06664d95ee9c61bed513419e0f645d7
Constantin Pape <c.pape@gmx.net>
2015-01-28 23:15:46 +0100

    added gitignore file

--- testground/test2.py
+++ testground/test2.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 if __name__ == '__main__':
-	A = np.ones((10,10))
-	np.savetxt("test",A)
+	A = np.ones((12,10))
+	
+	A[:,2] = 0.3784
+
+	np.savetxt("test",A,fmt='%6f')
 	
 	B = np.genfromtxt("test")
 
