Fixed Normalization of the Histograms in BayesClassifier, changed test procedure for words

c++_implementation/scowl_data/mk_data.py
@@ -44,33 +44,35 @@ def str_generator(length):
 
 
 def make_data(words, size):
-    """returns nparray containing size valid words and size notwords
-    """
-    
-    assert size <= len(words)
-    #initalize array for instances + class
-    data = np.zeros([2*size, len(words[0]) + 1], dtype=int)
-    
-    #fill one half with existing words...
-    for i in range(size):
-        data[i][-1] = 0                                 #label 0 will be generated
-        for j in range(len(words[i])):
-            data[i][j] = ord(words[i][j]) - ord('a')    #code: 'a' = 0
-    
-    #...the other half with random character strings (notwords)
-    gen = str_generator(len(words[0]))
-    for i in range(size, 2*size):
-        data[i][-1] = 1
-        notword = next(gen)
-        while notword in words:
-            notword = next(gen)
-        
-        for j in range(len(notword)):
-            data[i][j] = ord(notword[j]) - ord('a')
-    
-    #permutate and split into words + labels
-    data = np.random.permutation(data)
-    return data[:,:-1], data[:,-1]
+	"""returns nparray containing size valid words and size notwords
+	"""
+	
+	assert size <= len(words)
+	#initalize array for instances + class
+	data = np.zeros( (2*size, len(words[0]) + 1) )
+	
+	eps = 0.05
+	
+	#fill one half with existing words...
+	for i in range(size):
+	    data[i][-1] = 0                                 #label 0 will be generated
+	    for j in range(len(words[i])):
+			data[i][j] = ord(words[i][j]) - ord('a')    #code: 'a' = 0
+	
+	#...the other half with random character strings (notwords)
+	gen = str_generator(len(words[0]))
+	for i in range(size, 2*size):
+	    data[i][-1] = 1
+	    notword = next(gen)
+	    while notword in words:
+	        notword = next(gen)
+	    
+	    for j in range(len(notword)):
+			data[i][j] = ord(notword[j]) - ord('a')
+
+	#permutate and split into words + labels
+	data = np.random.permutation(data)
+	return data[:,:-1], data[:,-1]
 
 
 def mean_word_length():
@@ -88,28 +90,28 @@ def mean_word_length():
 
 
 if __name__ == '__main__':
-    #read in words
-    length = input("Choose length of words (default = 5): ")
-    if length == '':
-        length = 5
-    words = get_words(int(length))
-    print(len(words), "words of length", length, "loaded.")
-    
-    #set up training data
-    default = len(words)//2
-    size = input("Choose size of training data (default = " + str(default) + "): ")
-    if size == '':
-        size = default
-    train_data, train_labels = make_data(words, int(size))
-    test_data, test_labels = make_data(words, len(words))
-    print("eeeaasy")
-    
-    #save data to files
-    path="original/"
-    np.savetxt(path + "data_train.out", train_data, fmt='%i')
-    np.savetxt(path + "labels_train.out", train_labels, fmt='%i')
-    np.savetxt(path + "data_test.out", test_data, fmt='%i')
-    np.savetxt(path + "labels_test.out", test_labels, fmt='%i')
-    print("'.out' files save to " + path)
-    
-    
+	#read in words
+	length = input("Choose length of words (default = 5): ")
+	if length == '':
+	    length = 5
+	words = get_words(int(length))
+	print(len(words), "words of length", length, "loaded.")
+	
+	# set up data
+	data, labels = make_data( words, len(words) )
+	# split into training data (80 %) and test_data (20%)
+	split_indx 		= int( np.floor( 0.8*data.shape[0] ) )
+	train_data 		= data[:split_indx]
+	train_labels 	= labels[:split_indx]
+	test_data 		= data[split_indx+1:]
+	test_labels 	= labels[split_indx+1:]
+	
+	print("eeeaasy")
+	
+	#save data to files
+	path="original/"
+	np.savetxt(path + "data_train.out", 	train_data, 	fmt='%f')
+	np.savetxt(path + "labels_train.out", 	train_labels, 	fmt='%i')
+	np.savetxt(path + "data_test.out", 		test_data, 		fmt='%f')
+	np.savetxt(path + "labels_test.out", 	test_labels, 	fmt='%i')
+	print("'.out' files save to " + path)