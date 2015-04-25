052b487342242d0c07df0bcf6e604bae8439a5f5
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2015-04-25 13:37:37 +0200

    continued working on regression

--- ml2/ex1/log_regression.py
+++ ml2/ex1/log_regression.py
@@ -1,6 +1,7 @@
 import vigra 
 import numpy as np
 import sklearn
+from sklearn import cross_validation
 
 # load the dataset from sklearn and filter for 3s and 8s
 def load_data():
@@ -52,6 +53,7 @@ def test_sigmoid():
 # A = 1 x N
 # B = N x D
 def gradient(beta, X, y):
+	print X.shape, y.shape
 	assert(X.shape[0] == y.shape[0])
 	assert(X.shape[1] == beta.shape[0])
 	M   = y.shape[0]
@@ -105,12 +107,14 @@ def gradient_descent(X, y, m, beta_0, tau_0, gamma, mu):
 	return beta
 
 # stochastic gradient descent
-def gradient_descent(X, y, m, beta_0, tau_0, gamma, mu):
+def stochastic_gradient_descent(X, y, m, beta_0, tau_0, gamma, mu):
 	beta = beta_0
 	for t in range(m):
 		# choose a random training sample (sampling with replacement)
 		i = np.random.randint(0, X.shape[0])
-		beta -= tau_t(tau_0,gamma,1.,t)*gradient(beta,X[i],y[i])
+		X_i = np.expand_dims( np.array( X[i] ), axis = 0)
+		y_i = np.array( [ y[i] ] )
+		beta -= tau_t(tau_0,gamma,1.,t)*gradient(beta,X_i,y_i)
 	return beta
 
 # stochastic gradient descent with mini batch
@@ -135,12 +139,15 @@ def sg_momentum(X, y, m, beta_0, tau_0, gamma, mu):
 
 # average stochastic gradient
 def average_stochastic_gradient(X, y, m, beta_0, tau_0, gamma, mu):
+
 	beta = beta_0
 	g    = np.zeros(beta.shape)
 	for t in range(m):
 		# choose a random training sample (sampling with replacement)
 		i = np.random.randint(0, X.shape[0])
-		g -= tau_t(tau_0,gamma,.75,t)*gradient(g, X[i], y[i])
+		X_i = np.expand_dims( np.array( X[i] ), axis = 0)
+		y_i = np.array( [ y[i] ] )
+		g -= tau_t(tau_0,gamma,.75,t)*gradient(g, X_i, y_i)
 		beta = (1 - mu)*beta + mu*g
 	return beta
 
@@ -154,14 +161,16 @@ def stochastic_average_gradient(X, y, m, beta_0, tau_0, gamma, mu):
 		# choose a random training sample (sampling with replacement)
 		i = np.random.randint(0, X.shape[0])
 		d_old = d_mat[i]
-		d_mat[i] = gradient(beta,X[i],y[i])
+		X_i = np.expand_dims( np.array( X[i] ), axis = 0)
+		y_i = np.array( [ y[i] ] )
+		d_mat[i] = gradient( beta, X_i, y_i )
 		d += (d_mat[i] - d_old) / N
 		beta -= tau_t(tau_0, gamma, 1., t)*d 
 	return beta
 
 # dual coordinate ascent
 def dual_coordinate_ascent(X, y, m, beta_0, tau_0, gamma, mu):
-	alpha = np.random.uniform(X.shape[0])
+	alpha = np.random.uniform(size = (X.shape[0]) )
 	# init beta
 	beta = np.multiply(alpha,y).dot(X)
 	for t in range(m):
@@ -175,14 +184,45 @@ def dual_coordinate_ascent(X, y, m, beta_0, tau_0, gamma, mu):
 # weigthed least squares
 # TODO
 def weighted_least_squares(X, y, m, beta_0, tau_0, gamma, mu):
+	N = X.shape[0]
 	beta = beta_0
 	for t in range(m):
 		z = X.dot(beta)
-		V = np.sqrt( sigmoid(z).dot( np.ones(z.shape) - sigmoid(z) ) / N ) 
+		print z.shape
+		V = np.sqrt( sigmoid(z).dot( np.ones(z.shape) - sigmoid(z) ) / N )
+		print V.shape
+		_y = np.divide(y, sigmoid( np.multiply(y,z) ) )
+		_z = (z + _y).dot(V)
+		_X = X.dot(V)
+		# TODO use least square solver for this!
+		# beta = argmin_beta( _z - X beta)**2
 	return beta
 		
-		
-		
+# check if methods are working
+def test_methods(X,y):
+	methods = { "gradient_descent" : gradient_descent, "stochastic_gradient_descent" : stochastic_gradient_descent, 
+		"sg_minbatch" : sg_minibatch, "sg_momentum" : sg_momentum, "average_stochastic_gradient": average_stochastic_gradient, 
+		"stochastic_average_gradient" : stochastic_average_gradient, "dual_coordinate_ascent" : dual_coordinate_ascent,
+		"weighted_least_squares" : weighted_least_squares }
+
+	# make test/train split
+	X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+		X,y, test_size = 0.3, random_state = 0)
+	# test all methods with first row of parameters, just to check that they are working
+	tau = 0.001
+	mu = 0.1
+	gamma = 0.0001
+	beta_0 = np.zeros( X.shape[1] )
+	iterations = 5
+	for key in methods:
+		# weighted least sqaures not properly implemented yet!
+		if key == "weighted_least_squares":
+			continue
+		print key
+		beta_res = methods[key](X_train, y_train, iterations, beta_0, tau, gamma, mu)
+		prediction = predict(beta_res,X_test)
+		print "worked, loss =", zero_one_loss(prediction, y_test)
+
 
 if  __name__ == '__main__':
 	X, y = load_data()
@@ -191,3 +231,7 @@ if  __name__ == '__main__':
 	#print labels.shape
 	#test_sigmoid()
 	#test_gradient(X,y)
+	
+	test_methods(X,y)	
+		
+
