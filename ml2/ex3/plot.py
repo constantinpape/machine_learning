2c610278882e3c0680172d6c678f7c767a77fa3b
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2015-05-13 15:20:11 +0200

    Finished the report

--- ml2/ex3/plot.py
+++ ml2/ex3/plot.py
@@ -17,6 +17,16 @@ def plot_trainerror(data1, data2, data3, data4):
     plot.legend(loc = 'lower right')
     plot.show()
 
+def plot_single(data):
+    fig = plot.figure()
+
+    plot.plot( data[:,0], data[:,1], label = 'conv_aniso' )
+    plot.title('anisotropic convolutional network')
+    plot.xlabel('iteration')
+    plot.ylabel('error')
+    plot.legend(loc = 'lower right')
+    plot.show()
+
 def plot_greyscale(filt,name):
     fig = plot.figure()
 
@@ -31,16 +41,29 @@ if __name__ == '__main__':
     data_dropout = np.loadtxt('res/res_dropout_nn')
     data_prelu   = np.loadtxt('res/res_prelu_nn')
     data_conv = np.loadtxt('res/res_conv_nn')
+    data_aniso = np.loadtxt('res/res_conv_aniso_nn')
 
-    filter0 = np.load('res/filter/filter_5.npy')
-    filter1 = np.load('res/filter/filter_15.npy')
-    filter2 = np.load('res/filter/filter_30.npy')
+    # isotropic_filter
+    #filter0 = np.load('res/filter/filter_0.npy')
+    #filter1 = np.load('res/filter/filter_15.npy')
+    #filter2 = np.load('res/filter/filter_30.npy')
+
+    # anisotropic filter
+    filter0 = np.load('res/filters_aniso/filter_0.npy')
+    filter1 = np.load('res/filters_aniso/filter_15.npy')
+    filter2 = np.load('res/filters_aniso/filter_30.npy')
 
     trX, teX, trY, teY = mnist(ntrain = 10, ntest = 10)
 
     #plot_trainerror(data_nn,data_dropout,data_prelu,data_conv)
-    plot_greyscale(filter2, 'filter30')
+    #plot_single(data_aniso)
+    plot_greyscale(filter0, 'filter0')
     im = trX[5].reshape( (28,28) )
-    plot_greyscale(im, 'mnist')
+    conv = convolve(im, filter0)
+    plot_greyscale(conv, 'convolution')
+    plot_greyscale(filter1, 'filter15')
+    conv = convolve(im, filter1)
+    plot_greyscale(conv, 'convolution')
+    plot_greyscale(filter2, 'filter30')
     conv = convolve(im, filter2)
     plot_greyscale(conv, 'convolution')
